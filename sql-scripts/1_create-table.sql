966d5e05f60721ef4ac99d2a5891497c7a59db69
Pratik Dhoran <pratikdhoran2003@gmail.com>
2025-02-20 16:16:33 +0530

    sql-scripts added

---
+++ sql-scripts/1_create-table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    username VARCHAR(255) NOT NULL ,
+    email VARCHAR(255) UNIQUE NOT NULL ,
+    password VARCHAR(255) NOT NULL 
+);
+
+CREATE TABLE attendance(
+     id INT AUTO_INCREMENT PRIMARY KEY ,
+     user_id INT NOT NULL ,
+     check_in TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     check_out TIMESTAMP  NULL ,
+     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE 
+);   
